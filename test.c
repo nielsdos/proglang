d115b22de4b4c5a61b0ae280854497e6367b1450
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-03-21 21:08:50 +0100

    Update test code for fun

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
-extern double test_double(int64_t);
-extern int64_t test_int(int64_t, int64_t);
+extern int64_t factorial(int64_t);
 
 int main() {
-	printf("%g %ld\n", test_double(0), test_int(2, 8));
+	printf("%ld\n", factorial(6));
 	return 0;
 }
