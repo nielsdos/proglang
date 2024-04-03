58c2d8ed7e252677b1ec0cebaa9c98f8e5e20876
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-04-03 22:40:48 +0200

    Fixup test stability

--- tests/llvm/direct_call.pl
+++ tests/llvm/direct_call.pl
@@ -3,16 +3,16 @@
 fn foo() -> int:
   return bar()
 
-# CHECK: define i64 @foo() {
-# CHECK-NEXT: entry:
-# CHECK-NEXT:   %direct_call = call i64 @bar()
-# CHECK-NEXT:   ret i64 %direct_call
-# CHECK-NEXT: }
+# CHECK-DAG: define i64 @foo() {
+# CHECK-DAG: entry:
+# CHECK-DAG:   %direct_call = call i64 @bar()
+# CHECK-DAG:   ret i64 %direct_call
+# CHECK-DAG: }
 
 fn bar() -> int:
   return 1
 
-# CHECK: define i64 @bar() {
-# CHECK-NEXT: entry:
-# CHECK-NEXT:   ret i64 1
-# CHECK-NEXT: }
+# CHECK-DAG: define i64 @bar() {
+# CHECK-DAG: entry:
+# CHECK-DAG:   ret i64 1
+# CHECK-DAG: }
--- tests/llvm/indirect_call.pl
+++ tests/llvm/indirect_call.pl
@@ -3,19 +3,19 @@
 fn add_one(int x) -> int:
   return x + 1
 
-# CHECK: define i64 @add_one(i64 %0) local_unnamed_addr #{{[0-9]}} {
-# CHECK-NEXT: entry:
-# CHECK-NEXT:   %add = add i64 %0, 1
-# CHECK-NEXT:   ret i64 %add
-# CHECK-NEXT: }
+# CHECK-DAG: define i64 @add_one(i64 %0) local_unnamed_addr #{{[0-9]}} {
+# CHECK-DAG: entry:
+# CHECK-DAG:   %add = add i64 %0, 1
+# CHECK-DAG:   ret i64 %add
+# CHECK-DAG: }
 
 fn twice(fn (int) -> int callee, int x) -> int:
   return callee(x) + callee(x)
 
-# CHECK: define i64 @twice(ptr %0, i64 %1) local_unnamed_addr {
-# CHECK-NEXT: entry:
-# CHECK-NEXT:   %indirect_call = tail call i64 %0(i64 %1)
-# CHECK-NEXT:   %indirect_call6 = tail call i64 %0(i64 %1)
-# CHECK-NEXT:   %add = add i64 %indirect_call6, %indirect_call
-# CHECK-NEXT:   ret i64 %add
-# CHECK-NEXT: }
+# CHECK-DAG: define i64 @twice(ptr %0, i64 %1) local_unnamed_addr {
+# CHECK-DAG: entry:
+# CHECK-DAG:   %indirect_call = tail call i64 %0(i64 %1)
+# CHECK-DAG:   %indirect_call6 = tail call i64 %0(i64 %1)
+# CHECK-DAG:   %add = add i64 %indirect_call6, %indirect_call
+# CHECK-DAG:   ret i64 %add
+# CHECK-DAG: }
