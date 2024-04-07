5627a7146c7e258400f2c09f81ac1c1342e37a7b
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-04-07 17:29:14 +0200

    Cleanup named args typecheck test a bit

--- tests/type_checker/named_args.pl
+++ tests/type_checker/named_args.pl
@@ -6,22 +6,26 @@ fn test(int a, int b) -> int:
 fn caller1() -> int:
     return test(b=1, a = 3)
 
+# CHECK: Note: {{.+}}: first named argument passed here
+# CHECK-NEXT: {{.+}}: positional arguments must not come after named arguments
+
 fn caller2() -> int:
     return test(b = 1, 3)
+
+# CHECK-NEXT: {{.+}}: named arguments are not supported in indirect calls
 
 fn caller3() -> int:
     let t = test
     return t(b = 1, 3)
 
+# CHECK-NEXT: {{.+}}: argument 'c' not found in function 'test'
+
 fn caller4() -> int:
     return test(c = 1, d = 3)
+
+# CHECK-NEXT: {{.+}}: argument 'd' not found in function 'test'
 
 fn caller5() -> int:
     return test(a = 1, a = 3)
 
-# CHECK: Note: {{.+}}: first named argument passed here
-# CHECK-NEXT: {{.+}}: positional arguments must not come after named arguments
-# CHECK-NEXT: {{.+}}: named arguments are not supported in indirect calls
-# CHECK-NEXT: {{.+}}: argument 'c' not found in function 'test'
-# CHECK-NEXT: {{.+}}: argument 'd' not found in function 'test'
 # CHECK-NEXT: {{.+}}: argument 'a' already passed
